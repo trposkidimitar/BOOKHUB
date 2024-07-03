9ef471359a2781aa54f4c3dcf1970aef5b9925dc
Simeon-Ristovski <simeon.ristovski@students.finki.ukim.mk>
2024-07-03 16:39:59 +0200

    Added start form

---
+++ BookHub/BookHub/PocetnaForma.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BookHub
+{
+    partial class PocetnaForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PocetnaForma));
+            this.btnBiblioteka = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBiblioteka
+            // 
+            this.btnBiblioteka.BackColor = System.Drawing.Color.Gray;
+            this.btnBiblioteka.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBiblioteka.ForeColor = System.Drawing.Color.Black;
+            this.btnBiblioteka.Image = ((System.Drawing.Image)(resources.GetObject("btnBiblioteka.Image")));
+            this.btnBiblioteka.Location = new System.Drawing.Point(322, 243);
+            this.btnBiblioteka.Name = "btnBiblioteka";
+            this.btnBiblioteka.Size = new System.Drawing.Size(200, 200);
+            this.btnBiblioteka.TabIndex = 0;
+            this.btnBiblioteka.Text = "Библиотека";
+            this.btnBiblioteka.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBiblioteka.UseVisualStyleBackColor = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblName.Location = new System.Drawing.Point(299, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(682, 136);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "BOOK HUB";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(781, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 200);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Игра";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // PocetnaForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1262, 672);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnBiblioteka);
+            this.Name = "PocetnaForma";
+            this.Text = "PocetnaForma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBiblioteka;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button button1;
+    }
+}
